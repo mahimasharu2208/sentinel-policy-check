4f47742e15b9ba0de0506b793227af19fffb4f37
Mahima <mahima.sharu@hashicorp.com>
2025-09-30 13:39:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
